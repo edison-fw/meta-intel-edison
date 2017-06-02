b38c0fec27797b906593091fcb26badb6c526758
Ferry Toth <ftoth@exalondelft.nl>
2017-06-02 23:25:30 +0200

    post-install: remove parts that create damage on startup
    This tried to resize partitions and in the process removed our kernel. We keep the script for
    now as it might be usefull to add more appropriate one time initialization

--- meta-intel-edison-distro/recipes-core/post-install/files/post-install.sh
+++ meta-intel-edison-distro/recipes-core/post-install/files/post-install.sh
@@ -24,77 +24,12 @@ fi_echo () {
     printf "${msg_prefix}${1}${msg_suffix}\n"
 }
 
-# set_retry_count to failure file
-# arg $1 new retry count
-set_retry_count () {
-    fw_setenv first_install_retry $1
-}
-
-# get_retry_count from failure from bootloader
-get_retry_count () {
-    retry_count=$(fw_printenv first_install_retry | tr -d "first_install_retry=")
-    [ -z $retry_count ] && { set_retry_count 0; retry_count=0;}
-    return $retry_count
-}
-
-# exit first_install by rebooting and handling the failure by setting
-# the firmware target according to failure or success
-# on failure increment fail count and reboot
-# on success reboot in multi-user target
-# arg $1 exit code
-exit_first_install () {
-    if [ $1 -eq 0 ]; then
-        # reset failure count
-        set_retry_count 0
-        # update firmware target
-        # next reboot will be on multi-user target
-        fw_setenv bootargs_target multi-user
-    fi
-    # dump journal to log file
-    journalctl -u post-install -o short-iso >> /post-install.log
-    systemctl daemon-reload
-}
-
-# continue normal flow or exit on error code
-# arg $1 : return code to check
-# arg $2 : string resuming the action
-fi_assert () {
-    if [ $1 -ne 0 ]; then
-        fi_echo "${2} : Failed ret($1)" err;
-        exit_first_install $1;
-    else
-        fi_echo "${2} : Success";
-    fi
-}
-
-factory_partition () {
-    #unmount factory partition
-    systemctl stop factory.mount
-
-    mkdir -p /factory
-    mount /dev/disk/by-partlabel/factory /factory
-    # test can fail if done during manufacturing
-    if [ $? -ne 0 ];
-    then
-        mkfs.ext4 /dev/disk/by-partlabel/factory
-        mount /dev/disk/by-partlabel/factory /factory
-        echo "00:11:22:33:55:66" > /factory/bluetooth_address
-        echo "VSPPYWWDXXXXXNNN" > /factory/serial_number
-    fi
-}
-
-mount_home () {
-    # mount home partition on /home
-    mount /dev/disk/by-partlabel/home /home
-    fi_assert $? "Mount /home partition"
-}
 
 # generate sshd keys
 sshd_init () {
     rm -rf /etc/ssh/*key*
     systemctl start sshdgenkeys
 }
-
 
 # Substitute the SSID and passphrase in the file /etc/hostapd/hostapd.conf
 # The SSID is built from the hostname and a serial number to have a
@@ -127,75 +62,23 @@ setup_ap_ssid_and_passphrase () {
 # script main part
 
 # print to journal the current retry count
-get_retry_count
-retry_count=$?
-set_retry_count $((${retry_count} + 1))
-fi_echo "Starting Post Install (try: ${retry_count})"
+fi_echo "Starting Post Install"
+fi_echo "This should only run at first boot"
+fi_echo "The location of this file is meta-intel-edison-distro/recipes-core/post-install/files/post-install.sh"
 
 systemctl start blink-led
 
-ota_done=$(fw_printenv ota_done | tr -d "ota_done=")
-if [ "$ota_done" != "1" ];
-then
-    # backup initial /home/root directory
-    mkdir /tmp/oldhome
-    cp -R /home/* /tmp/oldhome/
-    fi_assert $? "Backup home/root contents of rootfs"
-
-    # format partition home to ext4
-    mkfs.ext4 -m0 /dev/disk/by-partlabel/home
-    fi_assert $? "Formatting home partition"
-
-    # mount home partition
-    mount_home
-
-    # copy back contents to /home and cleanup
-    cp -R /tmp/oldhome/* /home/
-    rm -rf /tmp/oldhome
-    fi_assert $? "Restore home/root contents on new /home partition"
-
-    # create a fat32 primary partition on all available space
-    echo -ne "n\np\n1\n\n\nt\nb\np\nw\n" | fdisk /dev/disk/by-partlabel/update
-
-    # silent error code for now because fdisk failed to reread MBR correctly
-    # MBR is correct but fdisk understand it as the main system MBR, which is
-    # not the case.
-    fi_assert 0 "Formatting update partition Step 1"
-
-    # create a loop device on update disk
-    losetup -o 8192 /dev/loop0 /dev/disk/by-partlabel/update
-    fi_assert $? "Formatting update partition Step 2"
-
-    # format update partition
-    mkfs.vfat /dev/loop0 -n "Edison" -F 32
-    fi_assert $? "Formatting update partition Step 3"
-
-    # remove loop device on update disk
-    losetup -d /dev/loop0
-    fi_assert $? "Formatting update partition Step 4 final"
-else
-    # just mount home partition after OTA update
-    mount_home
-fi
-
-# handle factory partition
-factory_partition
-
 # ssh
 sshd_init
-fi_assert $? "Generating sshd keys"
-
-# update entry in /etc/fstab to enable auto mount
-sed -i 's/#\/dev\/disk\/by-partlabel/\/dev\/disk\/by-partlabel/g' /etc/fstab
-fi_assert $? "Update file system table /etc/fstab"
+fi_echo $? "Generating sshd keys"
 
 # Setup Access Point SSID and passphrase
 setup_ap_ssid_and_passphrase
-fi_assert $? "Generating Wifi Access Point SSID and passphrase"
+fi_echo $? "Generating Wifi Access Point SSID and passphrase"
 
 fi_echo "Post install success"
 
 systemctl stop blink-led
 # end main part
-exit_first_install 0
+
 
