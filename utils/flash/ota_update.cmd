f41152a95e70beafccb84e262910e6227a9e741b
Razvan Becheriu <razvan@isc.org>
2019-09-30 09:04:16 +0300

    added support for OTA from pocky to thud and from thud to thud by updating boot environment

--- utils/flash/ota_update.cmd
+++ utils/flash/ota_update.cmd
@@ -3,6 +3,7 @@ echo === OTA update script ===
 setenv ota_done 0
 setenv ota_abort 0
 setenv ota_verbose 1
+setenv ota_alternative 0
 
 # Remove ota_abort_reason which is used to report error in case of failure
 env delete ota_abort_reason
@@ -14,14 +15,14 @@ env delete ota_abort_reason
 
 # function ota_conv_sizes
 # Convert a bytes size to a block size
-# input bytesize : size in bytes to convert
-# input blksize : size of a block in bytes
-# output numblk : converted size in blocks
+# input  bytesize : size in bytes to convert
+# input  blksize  : size of a block in bytes
+# output num_blk  : converted size in blocks
 setenv ota_conv_sizes 'setexpr num_blk $bytesize / $blksize ; setexpr mod_blk $bytesize % $blksize ; if itest $mod_blk > 0 ; then setexpr num_blk $num_blk + 1; fi;'
 
 # function ota_mmc_write
 # Write a memory buffer to mmc drive
-# input floadaddr     : address of buffer to write
+# input floadaddr    : address of buffer to write
 # input u_part_start : block start in mmc
 # input num_blk      : number of block to write
 setenv ota_mmc_write 'if itest $ota_verbose == 1 ; then echo "mmc write ${floadaddr} ${u_part_start} ${num_blk};"; fi; mmc write $floadaddr $u_part_start $num_blk; ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "mmc write ${floadaddr} ${u_part_start} ${num_blk} failed"; setenv ota_abort 1; fi;'
@@ -30,7 +31,7 @@ setenv ota_mmc_write 'if itest $ota_verbose == 1 ; then echo "mmc write ${floada
 # Load partition binary image from update partition to memory
 # input  ota_drive      : ota drive name
 # input  ota_image_name : filename of image to load
-# input  floadaddr       : memory address destination
+# input  floadaddr      : memory address destination
 # output filesize       : size of image in bytes
 setenv ota_load_image 'if itest $ota_verbose == 1 ; then echo "fatload ${ota_drive} ${floadaddr} ${ota_image_name};"; fi; fatload ${ota_drive} ${floadaddr} ${ota_image_name};ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "fatload ${ota_drive} ${floadaddr} ${ota_image_name} failed: ${ret}"; setenv ota_abort 1; fi;'
 
@@ -43,15 +44,21 @@ setenv ota_compute_hash 'if itest $ota_verbose == 1 ; then echo "hash sha1 $floa
 
 # function ota_test_image_and_partition_sizes
 # Test if image fit partition
-# input num_blk : image size in blocks
+# input num_blk   : image size in blocks
 # input u_part_sz : partition size in blocks
 setenv ota_test_image_and_partition_sizes 'if itest $num_blk > $u_part_sz ; then setenv ota_abort_reason "Partition ${u_part_lbl} too small for ${ota_image_name}"; setenv ota_abort 1; fi;'
 
 # function ota_find_partition
 # Find a partition by label
-# input u_part_lbl : partition label
+# input  u_part_lbl : partition label
 # output u_part_num : partition number
 setenv ota_find_partition 'if itest $ota_verbose == 1 ; then echo "part find mmc 0 label:${u_part_lbl} u_part_num;"; fi; part find mmc 0 label:${u_part_lbl} u_part_num;ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "part find mmc 0 label:${u_part_lbl} u_part_num failed: ${ret}"; setenv ota_abort 1; fi;'
+
+# function ota_find_partition_alternative
+# Find a partition by label
+# input  u_part_lbl : partition label
+# output u_part_num : partition number
+setenv ota_find_partition_alternative 'if itest $ota_verbose == 1 ; then echo "part number mmc 0 ${u_part_lbl} u_part_num;"; fi; part number mmc 0 ${u_part_lbl} u_part_num;ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "part number mmc 0 ${u_part_lbl} u_part_num failed: ${ret}"; setenv ota_abort 1; fi;'
 
 # function ota_get_partition_attributes
 # Retrieve partition attribute
@@ -61,25 +68,61 @@ setenv ota_find_partition 'if itest $ota_verbose == 1 ; then echo "part find mmc
 # output u_part_blksz : partition block size in bytes
 setenv ota_get_partition_attributes 'if itest $ota_verbose == 1 ; then echo "part info mmc 0:${u_part_num} u_part_start u_part_sz u_part_blksz;"; fi; part info mmc 0:${u_part_num} u_part_start u_part_sz u_part_blksz;ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "part info mmc 0:${u_part_num} u_part_start u_part_sz u_part_blksz failed: ${ret}"; setenv ota_abort 1; fi;'
 
+# function ota_get_partition_attributes_alternative
+# Retrieve partition attribute
+# input  u_part_lbl   : partition label
+# output u_part_start : partition start block number
+# output u_part_sz    : partition size in blocks
+# output u_part_blksz : partition block size in bytes
+setenv ota_get_partition_attributes_alternative 'if itest $ota_verbose == 1 ; then echo "part start mmc 0 ${u_part_lbl} u_part_start; part size mmc 0 ${u_part_lbl} u_part_sz; part block mmc 0 ${u_part_lbl} u_part_blksz;"; fi; part start mmc 0 ${u_part_lbl} u_part_start;ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "part start mmc 0 ${u_part_lbl} u_part_start failed: ${ret}"; setenv ota_abort 1; fi; part size mmc 0 ${u_part_lbl} u_part_sz;ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "part size mmc 0 ${u_part_lbl} u_part_sz failed: ${ret}"; setenv ota_abort 1; fi; part block mmc 0 ${u_part_lbl} u_part_blksz;ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "part block mmc 0 ${u_part_lbl} u_part_blksz failed: ${ret}"; setenv ota_abort 1; fi;'
+
 # function ota_exec_verbose_cmd
 # Execute a command when verbosity is set
 # input vb_cmd : contains code to run verbosily
 setenv ota_exec_verbose_cmd 'if itest $ota_verbose == 1 ; then run vb_cmd ; fi;'
 
+# function ota_use_alternative
+# Check uboot commands supported syntax
+# output ota_alternative : use alternative commands
+setenv ota_use_alternative 'if itest $ota_verbose == 1 ; then echo "Check support for commands alternative syntax"; fi; part find mmc 0 label:boot;ret=$?; if itest $ret != 0 ; then setenv ota_alternative 1; fi;'
+
+# function ota_update_env
+# Update env
+# input floadaddr : memory address destination
+# input filesize  : size of image in bytes
+setenv ota_update_env 'if itest $ota_verbose == 1 ; then echo "Update env"; fi; env import -c ${floadaddr} ${filesize};ret=$?; if itest $ret != 0 ; then setenv ota_abort_reason "env import -c ${floadaddr} ${filesize} failed: ${ret}"; setenv ota_abort 1; fi;'
 
 # function ota_cleans_script
 # remove from environment all variables and functions in this script
-setenv ota_cleans_script 'env delete -f sha1_sum sha1_sum_ref; env delete -f u_part_blksz u_part_lbl u_part_num u_part_start u_part_sz updt_part_num ota_drive; env delete -f mod_blk num_blk filesize bytesize blksize floadaddr vb_cmd; env delete -f floadaddr; env delete -f ota_conv_sizes ota_mmc_write ota_load_image ota_compute_hash; env delete -f ota_test_image_and_partition_sizes ota_find_partition ota_get_partition_attributes ota_exec_verbose_cmd ota_cleans_script;'
+setenv ota_cleans_script 'env delete -f sha1_sum sha1_sum_ref; env delete -f u_part_blksz u_part_lbl u_part_num u_part_start u_part_sz updt_part_num; env delete -f ota_drive ota_alternative; env delete -f mod_blk num_blk filesize bytesize blksize floadaddr vb_cmd; env delete -f floadaddr; env delete -f ota_conv_sizes ota_mmc_write ota_load_image ota_compute_hash; env delete -f ota_test_image_and_partition_sizes ota_find_partition ota_find_partition_alternative ota_get_partition_attributes ota_get_partition_attributes_alternative ota_exec_verbose_cmd ota_use_alternative ota_update_env ota_cleans_script;'
 
 # handle two pass validation of script
 if env exist ota_checked ; then if itest $ota_checked == 0; then echo "Ota previously failed retry"; fi; else setenv ota_checked 0; fi;
-if itest $ota_checked == 0; then echo "Validating Ota package" ; else echo "Processing Ota update"; fi;
+if itest $ota_checked == 0 ; then echo "Validating Ota package" ; else echo "Processing Ota update"; fi;
 
 setenv floadaddr 0x6400000
 
+run ota_use_alternative;
+
+if itest $ota_alternative == 1 ; then
+echo " "
+echo "Using alternative syntax"
+echo " "
+else
+echo " "
+echo "Using default syntax"
+echo " "
+fi
+
 # Find update partition on emmc drive
 setenv u_part_lbl update;
+
+if itest $ota_alternative == 1 ; then
+run ota_find_partition_alternative ;
+else
 run ota_find_partition ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 setenv ota_drive mmc 0:${u_part_num}
@@ -145,7 +188,7 @@ if itest $ota_checked == 1 ; then
     if itest $ret != 0 ; then setenv ota_abort_reason "mmc dev 0 2 failed: $ret"; setenv ota_abort 1; run ota_cleans_script; exit; fi;
 
     # Write image to partition
-    setenv u_part_start 0x00;
+    setenv u_part_start 0x0;
     run ota_mmc_write;
     if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
@@ -155,12 +198,14 @@ if itest $ota_checked == 1 ; then
     # Tell user about the status
     echo " "
     echo "Update of ${ota_image_name} to boot0 and boot1 partitions Done";
+    echo " "
 fi;
 
 # Start update of u-boot-edison.bin on u-boot0
 setenv ota_image_name u-boot-edison.bin
 setenv u_part_lbl u-boot0
 setenv sha1_sum_ref @@sha1_u-boot-edison.bin
+
 # Tell user what's going on
 echo " "
 if itest $ota_checked == 1 ; then
@@ -175,11 +220,21 @@ run ota_load_image;
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Find partition u-boot0
+if itest $ota_alternative == 1 ; then
+run ota_find_partition_alternative ;
+else
 run ota_find_partition ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Retrieve partition attribute
+if itest $ota_alternative == 1 ; then
+run ota_get_partition_attributes_alternative ;
+else
 run ota_get_partition_attributes ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Convert file size in byte to block
@@ -208,6 +263,7 @@ else
     # Tell user about the status
     echo " "
     echo "Update of ${ota_image_name} to ${u_part_lbl} partition  Done";
+    echo " "
 fi;
 
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
@@ -223,17 +279,28 @@ echo "Update of ${ota_image_name} to ${u_part_lbl} partition";
 else
 echo "Validating ${ota_image_name} hash for ${u_part_lbl} partition";
 fi;
+echo " "
 
 # Load partition binary image from update partition to memory
-run ota_load_image ;
+run ota_load_image;
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Find partition boot
+if itest $ota_alternative == 1 ; then
+run ota_find_partition_alternative ;
+else
 run ota_find_partition ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Retrieve partition attribute
+if itest $ota_alternative == 1 ; then
+run ota_get_partition_attributes_alternative ;
+else
 run ota_get_partition_attributes ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Convert file size in byte to block
@@ -262,8 +329,10 @@ else
     # Tell user about the status
     echo " "
     echo "Update of ${ota_image_name} to ${u_part_lbl} partition  Done";
+    echo " "
 fi;
 
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 # Start update of edison-image-edison.ext4 on rootfs
 setenv ota_image_name edison-image-edison.ext4
 setenv u_part_lbl rootfs
@@ -283,11 +352,21 @@ run ota_load_image;
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Find partition rootfs
+if itest $ota_alternative == 1 ; then
+run ota_find_partition_alternative ;
+else
 run ota_find_partition ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Retrieve partition attribute
+if itest $ota_alternative == 1 ; then
+run ota_get_partition_attributes_alternative ;
+else
 run ota_get_partition_attributes ;
+fi
+
 if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 
 # Convert file size in byte to block
@@ -298,8 +377,7 @@ run ota_conv_sizes ;
 setenv vb_cmd 'echo "Partition ${u_part_lbl} Start:0x${u_part_start} Size:0x${u_part_sz} BlockSize:0x${u_part_blksz}"; echo "${ota_image_name} size is 0x${filesize} bytes and 0x${num_blk} block"; echo " ";'
 run ota_exec_verbose_cmd ;
 
-
-if itest $ota_checked == 0; then
+if itest $ota_checked == 0 ; then
     # Test if image fit partition
     run ota_test_image_and_partition_sizes ;
     if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
@@ -307,7 +385,7 @@ if itest $ota_checked == 0; then
     # Verify data integrity
     run ota_compute_hash;
     if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
-    if itest.s $sha1_sum_ref != $sha1_sum ; then setenv ota_abort_reason "$ota_image_name bad hash $sha1_sum != $sha1_sum_ref"; setenv ota_abort 1; fi;
+    if itest.s $sha1_sum_ref != $sha1_sum ; then setenv ota_abort_reason "${ota_image_name} hash mismatch ($sha1_sum / $sha1_sum_ref)"; setenv ota_abort 1; fi;
     if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
 else
     # Write image to partition
@@ -317,12 +395,156 @@ else
     # Tell user about the status
     echo " "
     echo "Update of ${ota_image_name} to ${u_part_lbl} partition  Done";
+    echo " "
+fi;
+
+echo " "
+echo "Loading env";
+echo " "
+
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+# Start update of u-boot-envs/edison-blankcdc.bin on u-boot-env0
+setenv ota_image_name u-boot-envs/edison-blankcdc.bin
+setenv u_part_lbl u-boot-env0
+
+# Tell user what's going on
+echo " "
+if itest $ota_checked == 1 ; then
+echo "Update of ${ota_image_name} to ${u_part_lbl} partition";
+else
+echo "Validating ${ota_image_name} hash for ${u_part_lbl} partition";
+fi;
+echo " "
+
+# Load partition binary image from update partition to memory
+run ota_load_image;
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+# Find partition u-boot-env0
+if itest $ota_alternative == 1 ; then
+run ota_find_partition_alternative ;
+else
+run ota_find_partition ;
+fi
+
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+# Retrieve partition attribute
+if itest $ota_alternative == 1 ; then
+run ota_get_partition_attributes_alternative ;
+else
+run ota_get_partition_attributes ;
+fi
+
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+# Convert file size in byte to block
+setenv bytesize $filesize ;
+setenv blksize $u_part_blksz ;
+run ota_conv_sizes ;
+
+setenv vb_cmd 'echo "Partition ${u_part_lbl} Start:0x${u_part_start} Size:0x${u_part_sz} BlockSize:0x${u_part_blksz}"; echo "${ota_image_name} size is 0x${filesize} bytes and 0x${num_blk} block"; echo " ";'
+run ota_exec_verbose_cmd ;
+
+if itest $ota_checked == 0 ; then
+    # Test if image fit partition
+    run ota_test_image_and_partition_sizes ;
+    if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+else
+    # Write image to partition
+    run ota_mmc_write;
+    if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+    # Tell user about the status
+    echo " "
+    echo "Update of ${ota_image_name} to ${u_part_lbl} partition  Done";
+    echo " "
+fi;
+
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+# Start update of u-boot-envs/edison-blankcdc.bin on u-boot-env1
+setenv ota_image_name u-boot-envs/edison-blankcdc.bin
+setenv u_part_lbl u-boot-env1
+
+# Tell user what's going on
+echo " "
+if itest $ota_checked == 1 ; then
+echo "Update of ${ota_image_name} to ${u_part_lbl} partition";
+else
+echo "Validating ${ota_image_name} hash for ${u_part_lbl} partition";
+fi;
+echo " "
+
+# Load partition binary image from update partition to memory
+run ota_load_image;
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+# Find partition u-boot-env1
+if itest $ota_alternative == 1 ; then
+run ota_find_partition_alternative ;
+else
+run ota_find_partition ;
+fi
+
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+# Retrieve partition attribute
+if itest $ota_alternative == 1 ; then
+run ota_get_partition_attributes_alternative ;
+else
+run ota_get_partition_attributes ;
+fi
+
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+# Convert file size in byte to block
+setenv bytesize $filesize ;
+setenv blksize $u_part_blksz ;
+run ota_conv_sizes ;
+
+setenv vb_cmd 'echo "Partition ${u_part_lbl} Start:0x${u_part_start} Size:0x${u_part_sz} BlockSize:0x${u_part_blksz}"; echo "${ota_image_name} size is 0x${filesize} bytes and 0x${num_blk} block"; echo " ";'
+run ota_exec_verbose_cmd ;
+
+if itest $ota_checked == 0 ; then
+    # Test if image fit partition
+    run ota_test_image_and_partition_sizes ;
+    if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+else
+    # Write image to partition
+    run ota_mmc_write;
+    if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+    # Tell user about the status
+    echo " "
+    echo "Update of ${ota_image_name} to ${u_part_lbl} partition  Done";
+    echo " "
+fi;
+
+# Start update of u-boot-envs/edison-blankcdc.bin in env
+setenv ota_image_name u-boot-envs/edison-blankcdc.bin
+
+# Load partition binary image from update partition to memory
+run ota_load_image;
+if itest $ota_abort == 1 ; then run ota_cleans_script; exit ; fi;
+
+if itest $ota_checked == 0 ; then
+    # Skip env update
+    echo " "
+    echo "Skip env update"
+    echo " "
+else
+    # Update env
+    run ota_update_env
+    echo " "
+    echo "Update env  Done";
+    echo " "
 fi;
 
 run ota_cleans_script;
 
 echo " "
-if itest $ota_checked == 0; then
+if itest $ota_checked == 0 ; then
+    echo " "
     echo "Ota Pakage validated going to process it";
     setenv ota_checked 1;
     source $ota_script_addr;
