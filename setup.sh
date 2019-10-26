df3cab780b1993907127c56679d545957c8eb9d8
Ferry Toth <ftoth@exalondelft.nl>
2019-10-26 13:27:03 +0200

    sudo: apply patch to fix fetching sources
    Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

--- setup.sh
+++ setup.sh
@@ -348,6 +348,7 @@ COPYLEFT_LICENSE_INCLUDE = 'GPL* LGPL*'
   git apply $top_repo_dir/meta-intel-edison/utils/0001-Enable-SDKTAROPTS.patch
   cd $poky_dir
   git apply $top_repo_dir/meta-intel-edison/utils/0001-bitbake.conf-building-linux-yocto-5.2-requires-realp.patch
+  git apply $top_repo_dir/meta-intel-edison/utils/0001-sudo-Fix-fetching-sources.patch
 
   if [[ $my_sdk_host == win* ]]
   then
