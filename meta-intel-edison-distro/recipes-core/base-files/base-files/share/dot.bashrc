edison-fw/meta-intel-edison

commit 6f43f5cc16f6d1a15b5f898ae1448e34fedf9d12

base-files: colorize shell
Add some color to `ls` and shell prompt.

Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

meta-intel-edison-distro/recipes-core/base-files/base-files/share/dot.bashrc
@@ -1,0 +1,34 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+export PS1='\h:\w\$ '
+umask 022
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+xterm-color | xterm-256color)
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    ;;
+*)
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    ;;
+esac
+
+# You may uncomment the following lines if you want `ls' to be colorized:
+export LS_OPTIONS='--color=auto'
+eval `dircolors`
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+#
+Some more alias to avoid making mistakes:
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'

meta-intel-edison-distro/recipes-core/base-files/base-files_3.0.14.bbappend
@@ -2,7 +2,7 @@ FILESEXTRAPATHS_prepend := "${THISDIR}/base-files:"
 SRC_URI += "file://release"
 SRC_URI += "file://fstab"
 SRC_URI += "file://fstab.btrfs"
-SRC_URI += "file://share/dot.profile"
+#SRC_URI += "file://share/dot.profile"
 
 # override default volatile to suppress var/log link creation
 volatiles = "tmp"
@@ -11,6 +11,6 @@ do_install_append() {
 	install -m 0644 ${WORKDIR}/release ${D}${sysconfdir}/release
 	install -m 0644 ${WORKDIR}/fstab ${D}${sysconfdir}/fstab
 	install -m 0644 ${WORKDIR}/fstab.btrfs ${D}${sysconfdir}/fstab.btrfs
-	install -m 0755 ${WORKDIR}/share/dot.profile ${D}${sysconfdir}/skel/.profile
+#	install -m 0755 ${WORKDIR}/share/dot.profile ${D}${sysconfdir}/skel/.profile
 
 }

commit f9c048ca55b70d5e79b23676d453269eb550a349

base-files: colorize shell fix typo
fixes: 6f43f5cc1 ("base-files: colorize shell")
Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

meta-intel-edison-distro/recipes-core/base-files/base-files/share/dot.bashrc
@@ -27,8 +27,7 @@ eval `dircolors`
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
-#
-Some more alias to avoid making mistakes:
+# Some more alias to avoid making mistakes:
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'