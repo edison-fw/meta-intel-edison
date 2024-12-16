c343244aeb4faf08d6acd1c61b9651244bbf389c
Ferry Toth <ftoth@exalondelft.nl>
2024-12-16 22:42:55 +0100

    utils: altboot.sh to switch between kernels
    Move altboot.sh to the proper location. While here
    expand the syntax to make it suitable for variant (preempt-rt)
    kernels with the same version number.
    
    Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

---
+++ utils/flash/altboot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Use: ./altboot.sh 6.6.31-rt31 preempt-rt (or 6.3.31 standard)"
+echo "Switching to kernel ""$1""-edison-acpi-""$2"
+
+if [ ! -f "/boot/bzImage-""$1""-edison-acpi-""$2" ]; then
+    echo "/boot/bzImage-""$1""-edison-acpi-""$2"" does not exist, abort"
+    exit 1
+fi
+
+if [ ! -f "/boot/initrdNew-""$1" ]; then
+    echo "/boot/initrdNew-""$1"" does not exist, abort"
+    exit 1
+fi
+
+if [ ! -d "/lib/modules/""$1""-edison-acpi-""$2" ]; then
+    echo "/lib/modules/""$1""-edison-acpi-""$2"" does not exist, abort"
+    exit 1
+fi
+
+ln -sf "bzImage-""$1""-edison-acpi-""$2" /boot/bzImageNew
+ln -sf "initrdNew-""$1" /boot/initrdNew
+
+echo "Switched succesfully to kernel ""$1""-edison-acpi-""$2"
