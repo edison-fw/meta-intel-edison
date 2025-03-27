81ff6c061c0c5d5e801a0a463d1877c14588c61d
Ferry Toth <ftoth@exalondelft.nl>
2025-03-27 22:39:50 +0100

    utils: altboot.sh tag initrd with kernel type
    This allows to boot both preempt-rt and standard kernels AND initrd of the same version.
    Valid names:
    bzImage-6.13.0-edison-acpi-standard
    initrdNew-6.13.0-standard
    bzImage-6.14.0-rc6-edison-acpi-standard
    bzImage-6.12.3-edison-acpi-preempt-rt
    
    Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

--- utils/flash/altboot.sh
+++ utils/flash/altboot.sh
@@ -8,8 +8,8 @@ if [ ! -f "/boot/bzImage-""$1""-edison-acpi-""$2" ]; then
     exit 1
 fi
 
-if [ ! -f "/boot/initrdNew-""$1" ]; then
-    echo "/boot/initrdNew-""$1"" does not exist, abort"
+if [ ! -f "/boot/initrdNew-""$1""-""$2" ]; then
+    echo "/boot/initrdNew-""$1""-""$2"" does not exist, abort"
     exit 1
 fi
 
@@ -19,6 +19,6 @@ if [ ! -d "/lib/modules/""$1""-edison-acpi-""$2" ]; then
 fi
 
 ln -sf "bzImage-""$1""-edison-acpi-""$2" /boot/bzImageNew
-ln -sf "initrdNew-""$1" /boot/initrdNew
+ln -sf "initrdNew-""$1""-""$2" /boot/initrdNew
 
 echo "Switched succesfully to kernel ""$1""-edison-acpi-""$2"
